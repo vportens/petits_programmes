4ca6b3eb15882db8f2b8fcc2b05f6303faf6b712
Victor PORTENSEIGNE <viporten@e2r3p13.clusters.42paris.fr>
2022-03-20 19:34:13 +0100

    add score fiche

--- 2048/incs/game.h
+++ 2048/incs/game.h
@@ -7,7 +7,7 @@
 
 enum e_const
 {
-	WIN_VALUE = 1
+	WIN_VALUE = 32
 };
 
 
--- 2048/srcs/play_game.c
+++ 2048/srcs/play_game.c
@@ -6,7 +6,7 @@
 /*   By: viporten <viporten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 16:04:59 by marnaudy          #+#    #+#             */
-/*   Updated: 2022/03/20 19:25:11 by viporten         ###   ########.fr       */
+/*   Updated: 2022/03/20 19:32:50 by viporten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,8 +111,12 @@ int	play_game(int size, char *name)
 			return (score);
 		add_number(board, size);
 		if (is_game_over(board, size))
+		{
+			game_over_screen(score, name);
 			break ;
+		}
 	//		return (game_over_screen(score, name));
 	}
+	is_high_score(size, score, name, "score.txt");
 	return (score);
 }
